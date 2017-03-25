d55e3f400a0f108e7904ab2ed02e01f435e11091
Vinícius Goulart <goulart.vs@gmail.com>
2017-03-25 20:17:00 -0300

    Release v1.0.0

--- index.js
+++ index.js
@@ -5,11 +5,45 @@ function GenerateIncludesWebpackPlugin() {}
 
 GenerateIncludesWebpackPlugin.prototype.apply = function(compiler) {
   compiler.plugin("emit", function(compilation, callback) {
-    compilation.chunks.forEach(function(chunk) {
-      chunk.files.forEach(function(filename) {
-        console.log(filename);
-      });
-    });
+    let head = "";
+    let body = "";
+
+    let publicPath = compilation.mainTemplate.getPublicPath({
+      hash: compilation.hash
+    }) || "";
+    if (publicPath && publicPath.substr(-1) !== "/") {
+      publicPath += "/";
+    }
+
+    for (const chunk of compilation.chunks) {
+      if (chunk.name) {
+        for (const filename of chunk.files) {
+          if (filename.match(/\.css$/)) {
+            head += `link(href="${publicPath + filename}", rel="stylesheet")\n`;
+          } else if (filename.match(/\.js$/)) {
+            body += `script(type="text/javascript", src="${publicPath + filename}")\n`;
+          }
+        }
+      }
+    }
+
+    compilation.assets["head.pug"] = {
+      source: function() {
+        return head;
+      },
+      size: function() {
+        return head.length;
+      }
+    };
+
+    compilation.assets["body.pug"] = {
+      source: function() {
+        return body;
+      },
+      size: function() {
+        return body.length;
+      }
+    };
 
     callback();
   });
